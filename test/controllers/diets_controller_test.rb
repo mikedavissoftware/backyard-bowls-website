664a76e2a8496a6f8818438d0bde4ad7b17436b8
Michael Davis <micdavis93@gmail.com>
2024-02-06 10:54:38 -0700

    diets controller test done

--- test/controllers/diets_controller_test.rb
+++ test/controllers/diets_controller_test.rb
@@ -6,18 +6,19 @@ class DietsControllerTest < ActionDispatch::IntegrationTest
     @vegan = diets(:vegan)
   end
 
-  test "INDEX should return all diets" do
+  test "INDEX should respond with all diets, REGARDLESS OF LOGIN" do
     get diets_path
-    assert_response :success
+    assert_response :success, "did not respond with success code 200"
     object = JSON.parse(response.body)
-    assert object.length == 5
+    assert object.length == 5, "responded with incorrect number of diets"
+    assert object[3]["diet"] == @vegan.diet, "unexpected value for diet on chosen diet"
   end
 
-  test "SHOW should return one diet" do
+  test "SHOW should respond with one diet, REGARDLESS OF LOGIN" do
     get diet_path(@vegan)
-    assert_response :success
+    assert_response :success, "did not respond with success code 200"
     object = JSON.parse(response.body)
-    assert object["diet"] == @vegan.diet
+    assert object["diet"] == @vegan.diet, "unexpected value for diet on diet"
   end
 
 end
