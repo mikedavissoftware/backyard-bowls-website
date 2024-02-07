09589cd9cfba6751041ad17be59b5283b9b894a7
Michael Davis <micdavis93@gmail.com>
2024-02-06 21:26:06 -0700

    simplified sessions controller test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,18 +2,12 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @username = "sam"
-    @password = "foobar"
-    login(@username, @password)
-  end
-
   test "SHOW should return current session's user info" do
+    login()
     get "/me"
     assert_response :success, "did not respond with success code 200"
     object = JSON.parse(response.body)
-    assert object['username'] == @username, "unexpected value for login username"
+    assert object['username'] == "frodo", "unexpected value for login username"
   end
-
 
 end
